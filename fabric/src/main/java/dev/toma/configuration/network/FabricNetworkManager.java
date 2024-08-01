7ec903475943b7fc7b745c9e4e908151ffe54539
Toma <novotny.tom96@gmail.com>
2024-08-01 22:09:04 +0200

    Fix fabric server launch

--- fabric/src/main/java/dev/toma/configuration/network/FabricNetworkManager.java
+++ fabric/src/main/java/dev/toma/configuration/network/FabricNetworkManager.java
@@ -1,7 +1,7 @@
 package dev.toma.configuration.network;
 
-import dev.toma.configuration.network.message.S2C_SendConfigDataMessage;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import dev.toma.configuration.Configuration;import dev.toma.configuration.network.message.S2C_SendConfigDataMessage;
+import net.fabricmc.api.EnvType;import net.fabricmc.api.Environment;import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
@@ -14,12 +14,19 @@ public class FabricNetworkManager implements NetworkManager {
     public void registerMessages() {
         PayloadTypeRegistry.playS2C().register(S2C_SendConfigDataMessage.TYPE, S2C_SendConfigDataMessage.CODEC);
 
-        ClientPlayNetworking.registerGlobalReceiver(S2C_SendConfigDataMessage.TYPE, (payload, context) -> payload.receive());
+        if (Configuration.PLATFORM.getEnvironment() == dev.toma.configuration.config.Environment.CLIENT) {
+            this.registerClientReceivers();
+        }
     }
 
     @Override
     public void dispatchClientMessage(ServerPlayer player, CustomPacketPayload message) {
         ServerPlayNetworking.send(player, message);
+    }
+
+    @Environment(EnvType.CLIENT)
+    private void registerClientReceivers() {
+        ClientPlayNetworking.registerGlobalReceiver(S2C_SendConfigDataMessage.TYPE, (payload, context) -> payload.receive());
     }
 
     private FabricNetworkManager() {}
